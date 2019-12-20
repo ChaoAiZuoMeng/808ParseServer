4f00bdfa4b365a27a1ea9ccfc67f4f4f46fbd67c
thymeleaf <13961809519@163.com>
2019-12-20 15:05:15 +0800

    byteToHex

--- src/main/java/com/chaokong/tool/Transfer.java
+++ src/main/java/com/chaokong/tool/Transfer.java
@@ -8,6 +8,22 @@ public class Transfer {
 			'6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
 
+
+
+	/**
+	 * 字节转十六进制
+	 * @param b 需要进行转换的byte字节
+	 * @return  转换后的Hex字符串
+	 */
+	public static String byteToHex(byte b){
+		String hex = Integer.toHexString(b & 0xFF);
+		if(hex.length() < 2){
+			hex = "0" + hex;
+		}
+		return hex;
+	}
+	
+
 	public static String bytesToHex(byte[] bytes) {
 		char[] buf = new char[bytes.length * 2];
 		int index = 0;
